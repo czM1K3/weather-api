805cd61c7c1e33389c34d37132d66c1ef918e403
czM1K3 <misamadera1@gmail.com>
2024-09-10 21:05:44 +0200

    Init rust

---
+++ src/chmi_api.rs
@@ -0,0 +1,66 @@
+use serde::Serialize;
+use surf::{get, StatusCode};
+use image::{DynamicImage, ImageOutputFormat};
+use std::io::Cursor;
+use image::imageops::crop_imm;
+use chrono::prelude::*;
+use chrono::DateTime;
+
+#[derive(Serialize)]
+pub struct LabelUrl {
+    label: String,
+    url: String,
+}
+
+fn f (input: u32) -> String {
+    if input <= 9 {
+        return format!("0{}", input);
+    }
+    format!("{}", input)
+}
+
+pub async fn get_all(past: i64, url: &String) -> Vec<LabelUrl> {
+    let current_date = Utc::now().to_utc();
+    let current_time = ((current_date.timestamp_millis() as f64 / 1000 as f64/ 60 as f64/ 5 as f64).floor() as i64) * 5 * 60 * 1000;
+    let mut last_possible_date = DateTime::from_timestamp_millis(current_time.into()).unwrap();
+    let possible_image = get_image(
+        last_possible_date.year().try_into().unwrap(),
+        last_possible_date.month().try_into().unwrap(),
+        last_possible_date.day().try_into().unwrap(),
+        last_possible_date.hour().try_into().unwrap(),
+        last_possible_date.minute().try_into().unwrap()
+    ).await;
+    if possible_image == None {
+        last_possible_date = DateTime::from_timestamp_millis(last_possible_date.timestamp_millis() - 5 * 60 * 1000).unwrap();
+    }
+
+    let mut arr: Vec<LabelUrl> = Vec::new();
+    for i in 0..past {
+        let date = DateTime::from_timestamp_millis(last_possible_date.timestamp_millis() - i * 5 * 60 * 1000).unwrap();
+        let local_date: DateTime<Local> = DateTime::from(date);
+        arr.push(LabelUrl {
+            label: format!("{}:{}", local_date.hour(), f(local_date.minute())),
+            url: format!("{}/api/get/{}/{}/{}/{}/{}", url, date.year(), date.month(), date.day(), date.hour(), date.minute()),
+        });
+    }
+    arr
+}
+
+pub async fn get_image(year: u16, month: u8, day: u8, hour: u8, minute: u8) -> Option<Vec<u8>> {
+    let url = format!("https://www.chmi.cz/files/portal/docs/meteo/rad/inca-cz/data/czrad-z_max3d/pacz2gmaps3.z_max3d.{}{}{}.{}{}.0.png", year, f(month.into()), f(day.into()), f(hour.into()), f(minute.into()));
+
+    let mut response = get(url).await.expect("Failed to download image");
+    if response.status() != StatusCode::Ok {
+        return None;
+    }
+    let data = response.body_bytes().await.expect("Failed to get body");
+
+    let img = image::load_from_memory(data.as_slice()).unwrap();
+    let img_resized = crop_imm(&img, 1, 95, 597, 320);
+    let img_resized_converted = DynamicImage::ImageRgba8(img_resized.to_image());
+    
+    let mut buf = Vec::new();
+    img_resized_converted.write_to(&mut Cursor::new(&mut buf), ImageOutputFormat::Png)
+        .expect("Failed to write image to buffer");
+    Some(buf)
+}
---
+++ src/main.rs
@@ -0,0 +1,107 @@
+use chrono::NaiveDate;
+use rocket::http::{ContentType, Header};
+use rocket::response::{Responder, Response};
+use rocket::Request;
+use std::io::Cursor;
+use chrono::prelude::*;
+use rocket::State;
+
+mod chmi_api;
+
+pub use crate::chmi_api::get_image;
+pub use crate::chmi_api::get_all;
+
+#[macro_use] extern crate rocket;
+
+// Generated with ChatGPT
+pub struct CustomBinaryResponse {
+    data: Vec<u8>,
+    content_type: ContentType,
+    custom_headers: Vec<Header<'static>>,
+}
+impl<'r> Responder<'r, 'static> for CustomBinaryResponse {
+    fn respond_to(self, _request: &'r Request<'_>) -> rocket::response::Result<'static> {
+        let mut response = Response::build();
+
+        // Set the content type
+        response.header(self.content_type);
+
+        // Add custom headers
+        for header in self.custom_headers {
+            response.header(header);
+        }
+
+        // Set the body as a cursor of the binary data
+        response.sized_body(self.data.len(), Cursor::new(self.data));
+
+        response.ok()
+    }
+}
+// End of generated with ChatGPT
+
+struct GlobalState {
+    pub past: i64,
+    pub url: String,
+}
+
+impl GlobalState {
+    pub fn new() -> Self {
+        let past = match std::env::var("PAST") {
+            Ok(x) => x.parse::<i64>().unwrap_or(24),
+            _ => 24,
+        };
+        GlobalState {
+            past,
+            url: std::env::var("URL").unwrap_or(format!("http://localhost:8000")),
+        }
+    }
+}
+
+#[get("/api/get")]
+async fn get_list(state: &State<GlobalState>) -> CustomBinaryResponse {
+    let arr = get_all(state.past, &state.url).await;
+    let json = serde_json::to_string(&arr).unwrap();
+    CustomBinaryResponse {
+        data: json.as_bytes().to_vec(),
+        content_type: ContentType::JSON,
+        custom_headers: vec![],
+    }
+}
+
+#[get("/api/get/<year>/<month>/<day>/<hour>/<minute>")]
+async fn get_single(year: u16, month: u8, day: u8, hour: u8, minute: u8, state: &State<GlobalState>) -> Option<CustomBinaryResponse> {
+    if year < 2000 || month < 1 || month > 12  || day < 1 || day > 31 || hour > 23 || minute > 59 || minute % 5 != 0 {
+        return None
+    }
+
+    let requested_date = NaiveDate::from_ymd_opt(year.into(), month.into(), day.into())?.and_hms_opt(hour.into(), minute.into(), 0)?.and_utc(); 
+    let requested_time = requested_date.timestamp_millis() / 1000 / 60;
+    let current_date = Utc::now().to_utc();
+    let current_time = current_date.timestamp_millis() / 1000 / 60;
+
+    if requested_time > current_time || requested_time < current_time - (state.past * 5) - 1 {
+        return None;
+    }
+
+    let image = get_image(year, month, day, hour, minute).await;
+    match image {
+        Some(data) => Some(CustomBinaryResponse {
+            data,
+            content_type: ContentType::Binary,
+            custom_headers: vec![
+                Header::new("Content-Type", "image/png"),
+                Header::new("Content-Disposition", format!("attachment; filename=meteo-{}-{}-{}-{}-{}.png", year, month, day, hour, minute)),
+            ],
+        }),
+        None => None
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .manage(GlobalState::new())
+        .mount("/", routes![get_list])
+        .mount("/", routes![get_single])
+}
+
